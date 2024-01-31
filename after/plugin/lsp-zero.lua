0819470e9348e9afcd8704f5461499783ef0108e
Cameron Reed <cameron@cam123.dev>
2024-01-30 21:13:47 -0700

    Initial commit

---
+++ after/plugin/lsp-zero.lua
@@ -0,0 +1,31 @@
+local lsp_zero_config = {
+  call_servers = 'global',
+}
+
+local lsp_servers = {
+  'zls',
+  'nixd',
+  'gopls',
+  'pylsp',
+  'templ',
+  'ansiblels',
+  'rust_analyzer',
+}
+
+vim.filetype.add({ extension = { templ = "templ" } })
+
+
+local lsp = require('lsp-zero').preset({})
+lsp.set_preferences(lsp_zero_config)
+
+lsp.on_attach(function(_, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+lsp.setup_servers(lsp_servers)
+
+local lsp_config = require('lspconfig')
+lsp_config.lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
+
---
+++ lua/cam123/init.lua
@@ -0,0 +1,4 @@
+require("cam123.set")
+require("cam123.remap")
+require("cam123.lazy")
+
