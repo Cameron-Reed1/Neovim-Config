9dcf968446a8bf9c1ed73eaf98f58475e0ec752f
Cameron Reed <cameron@cam123.dev>
2024-10-31 12:43:57 -0600

    Keybinds to run project

--- lua/cam123/remap.lua
+++ lua/cam123/remap.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<Escape>", function () vim.opt.hlsearch = false end)
+
+vim.keymap.set("n", "<leader>pr", "<cmd>!just run<cr>")
+vim.keymap.set("n", "<leader>pb", "<cmd>!just build<cr>")
